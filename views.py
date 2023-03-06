3724e147d96babc7ee23d0085121e1ba6ed2e708
milanNavodya <milannavodya70@gmail.com>
2023-03-06 18:09:09 +0530

    [ADDED] - 404 page not found webpage [UPDATED] - Doctor profile view, give access to views for related users

--- views.py
+++ views.py
@@ -3,6 +3,7 @@ from flask_login import login_required
 from config import db
 from datetime import datetime
 import routes
+from models import User
 
 views = Blueprint('views', __name__)
 
@@ -62,41 +63,76 @@ def index_user():
 @views.route('/user/setting')
 @login_required
 def settings_user_profile():
-    return render_template('patient_profile_settings.html')
+    user_id = session.get('id')
+    user = User.query.filter_by(id=user_id).first()
+    if user.user_role == 'patient':
+        return render_template('patient_profile_settings.html')
+    else:
+        return redirect(url_for('views.page_not_found'))
 
 
 @views.route('/user/doctor')
 @login_required
 def doctor_for_user():
-    return render_template('user_doctor_profile.html')
+    user_id = session.get('id')
+    user = User.query.filter_by(id=user_id).first()
+    if user.user_role == 'patient':
+        return render_template('user_doctor_profile.html')
+    else:
+        return redirect(url_for('views.page_not_found'))
 
 
 @views.route('/user/buy-med')
 @login_required
 def buy_medicine():
-    return render_template('patient_buy_medicine.html')
+    user_id = session.get('id')
+    user = User.query.filter_by(id=user_id).first()
+    if user.user_role == 'patient':
+        return render_template('patient_buy_medicine.html')
+    else:
+        return redirect(url_for('views.page_not_found'))
 
 
 @views.route('/user/appointment', methods=['POST'])
 @login_required
 def place_appointment():
-    return render_template('appointment_booking.html')
+    user_id = session.get('id')
+    user = User.query.filter_by(id=user_id).first()
+    if user.user_role == 'patient':
+        return render_template('appointment_booking.html')
+    else:
+        return redirect(url_for('views.page_not_found'))
 
 
 # Routes for Pharmacists
 @views.route('/pharmacist')
 @login_required
 def index_pharmacist():
-    return render_template('pharmacy_profile.html')
+    user_id = session.get('id')
+    user = User.query.filter_by(id=user_id).first()
+    if user.user_role == 'pharmacist':
+        return render_template('pharmacy_profile.html')
+    else:
+        return redirect(url_for('views.page_not_found'))
 
 
 @views.route('/pharmacist/sales')
 @login_required
 def sale_order_details():
-    return render_template('pharmacy_sales.html')
+    user_id = session.get('id')
+    user = User.query.filter_by(id=user_id).first()
+    if user.user_role == 'pharmacist':
+        return render_template('pharmacy_sales.html')
+    else:
+        return redirect(url_for('views.page_not_found'))
 
 
-@views.route('/pharmacist/sales')
+@views.route('/pharmacist/inventory')
 @login_required
 def pharmacy_inventory():
     return render_template('pharmacy_inventory.html')
+
+
+@views.route('/page-not-found')
+def page_not_found():
+    return render_template('404-error.html')
